31bf04a98dba01ccdad183044e44f0606ce73e90
micahjmartin <mjm5097@rit.edu>
2017-04-28 03:09:31 -0400

    Added Header file

---
+++ demo.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int getInput(char *buff, int len);
+
+void cleanInput(char *buff);
+
+int getName(char *buff);
+
+int getCCN(char *buff);
+
+int getExp(char *buff);
+
+int getZip(char *buff);
+
+int getPrice(char *buff);
+
+void cleanup(int i);
+
+int main(void);
