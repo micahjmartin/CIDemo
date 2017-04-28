559efb6f4197549c69aedb547beac1537e202955
micahjmartin <mjm5097@rit.edu>
2017-04-28 03:10:09 -0400

    Added Checks for CCN and Name

--- demo.c
+++ demo.c
@@ -2,10 +2,29 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include "demo.h"
+
+#define FINISH {retval = 1; goto CLEANUP;}
 #define BUFF_LEN 500
 #define CCN_LEN 16+3+1+1
 #define EXP_LEN 9+1+4+1+1
 #define ZIP_LEN 5+1+4+1+1
+
+int getInput(char *buff, int len)
+{
+    fgets(buff, len, stdin);
+    if(strchr(buff, '\n') == NULL)
+    {
+	int ex;
+	while ((ex = fgetc(stdin)) != '\n' && ex != EOF)
+	{
+	    ;
+	}
+    }
+    //fflush(stdin);
+    cleanInput(buff);
+    return 0;
+}
 
 void cleanInput(char *buff)
 {
@@ -18,10 +37,14 @@ void cleanInput(char *buff)
 
 int getName(char *buff)
 {
-
-    printf("Enter your name: ");
-    fgets(buff, BUFF_LEN, stdin);
-    cleanInput(buff);
+    printf("Enter your first and last name: ");
+    getInput(buff,BUFF_LEN);
+    
+    if(strchr(buff,' ') == NULL)
+    {
+	printf("Does not contain two names\n");
+	return 1;
+    }
     return 0;
 }
 
@@ -29,8 +52,33 @@ int getName(char *buff)
 int getCCN(char *buff)
 {
     printf("Enter your credit card number: ");
-    fgets(buff, CCN_LEN, stdin);
-    cleanInput(buff);
+    getInput(buff,CCN_LEN);
+    char ch;
+    int dashes = 0; // get the number of dashes in the string. 
+    if(strlen(buff) != 19)
+    {
+	printf("Invalid length of credit card number\n",ch);
+	return 1;
+    }
+    for(unsigned int i = 0; i < strlen(buff); i++)
+    {
+	ch = buff[i];
+	if(ch == '-')
+	    dashes++;
+	if(ch != '0' && ch != '1' && ch != '2' && ch != '3' && ch != '4' &&
+	    ch != '5' && ch != '6' && ch != '7' && ch != '8' && ch != '9' &&
+	    ch != '-')
+	{
+	    printf("Invalid character [%c]\n",ch);
+	    return 1;
+	}
+
+    }
+    if(dashes != 3)
+    {
+	printf("Invalid: Please follow the following format: xxxx-xxxx-xxxx-xxxx");
+	return 1;
+    }
     return 0;
 }
 
@@ -60,24 +108,16 @@ int getPrice(char *buff)
     return 0;
 }
 
-int getInput(char *buff, int len)
+
+void cleanup(int i)
 {
-    fgets(buff, len, stdin);
-    if(strchr(buff, '\n') == NULL)
-    {
-	int ex;
-	while ((ex = fgetc(stdin)) != '\n' && ex != EOF)
-	{
-	    ;
-	}
-    }
-    //fflush(stdin);
-    cleanInput(buff);
-    return 0;
+    // Free all the buffers
+    exit(i);
 }
 
 int main(void)
 {
+    int retval = 0;
     // Allocate all the buffers for the information
     char *name = calloc(sizeof(char), BUFF_LEN);
     char *ccn = calloc(sizeof(char), CCN_LEN);
@@ -88,10 +128,11 @@ int main(void)
 
 
 
-    printf("Enter your name: ");
-    getInput(name,BUFF_LEN);
-    printf("Enter your Credit Card Number: ");
-    getInput(ccn,CCN_LEN);
+    if (getName(name) != 0)
+	FINISH
+    if (getCCN(ccn) != 0)
+	FINISH
+
     printf("Enter your Expiration month and year: ");
     getInput(exp,EXP_LEN);
     printf("Enter your zip code: ");
@@ -107,14 +148,13 @@ int main(void)
     printf("Zip Code: %s\n", zip);
     printf("Purchase Amount: %s\n", price);
     printf("Email Address: %s\n", email);
-    
-    // Free all the buffers
+
+    CLEANUP: 
     free(name);
     free(ccn);
     free(exp);
     free(zip);
     free(price);
     free(email);
-    
-    return 0;
+    return retval;
 }
